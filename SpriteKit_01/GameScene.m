059d8033638cc6cfa57c027bdef5adc6e5651999
Michael Vilabrera <m.vilabrera@gmail.com>
2015-07-08 11:14:00 -0400

    added paddle, ball sprite disappears from view

--- SpriteKit_01/GameScene.m
+++ SpriteKit_01/GameScene.m
@@ -8,25 +8,15 @@
 
 #import "GameScene.h"
 
+@interface GameScene()
+
+@property (nonatomic) SKSpriteNode *paddle;
+
+@end
+
 @implementation GameScene
 
-/*
- 
- NOTES:
- old tutorials use the method -(id)initWithSize:(CGSize)size
- new ones may take the same code in -(void)didMoveToView:(SKView *)view
- 
- */
-
--(void)didMoveToView:(SKView *)view {
-    /* Setup your scene here */
-    self.backgroundColor = [SKColor whiteColor];
-    
-    self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
-    
-    // change gravity settings
-    self.physicsWorld.gravity = CGVectorMake(0, 0);
-    
+- (void)addBall {
     // new sprite from the ball
     SKSpriteNode *ball = [SKSpriteNode spriteNodeWithImageNamed:@"ball"];
     
@@ -48,8 +38,57 @@
     [ball.physicsBody applyImpulse:simpleVector];
 }
 
+- (void)addPlayer {
+    // create paddle
+    self.paddle = [SKSpriteNode spriteNodeWithImageNamed:@"paddle"];
+    // position it
+    self.paddle.position = CGPointMake(self.size.width/2, 100);
+    // add a physics body
+    self.paddle.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.paddle.frame.size];
+    // make paddle static
+    self.paddle.physicsBody.dynamic = NO;
+    // add to scene
+    [self addChild:self.paddle];
+}
+
+/*
+ 
+ NOTES:
+ old tutorials use the method -(id)initWithSize:(CGSize)size
+ new ones may take the same code in -(void)didMoveToView:(SKView *)view
+ 
+ */
+
+-(void)didMoveToView:(SKView *)view {
+    /* Setup your scene here */
+    self.backgroundColor = [SKColor whiteColor];
+    
+    self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+    
+    // change gravity settings
+    self.physicsWorld.gravity = CGVectorMake(0, 0);
+    
+    [self addBall];
+    [self addPlayer];
+}
+
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
+}
+
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    for (UITouch *touch in touches) {
+        CGPoint location = [touch locationInNode:self];
+        CGPoint nextPosition = CGPointMake(location.x, 100);
+        self.paddle.position = nextPosition;
+        // stop paddle from going too far
+        if (nextPosition.x < self.paddle.size.width / 2) {
+            nextPosition.x = self.paddle.size.width / 2;
+        }
+        if (nextPosition.x > self.size.width - (self.paddle.size.width / 2)) {
+            nextPosition.x = self.size.width - (self.paddle.size.width / 2);
+        }
+    }
 }
 
 @end
