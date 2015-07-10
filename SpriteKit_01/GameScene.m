138a7df02c6e836709a4b81b42c8b4e695e33189
Michael Vilabrera <m.vilabrera@gmail.com>
2015-07-10 12:15:05 -0400

    completed reacting to collisions

--- SpriteKit_01/GameScene.m
+++ SpriteKit_01/GameScene.m
@@ -8,13 +8,50 @@
 
 #import "GameScene.h"
 
-@interface GameScene()
+@interface GameScene() <SKPhysicsContactDelegate>
 
 @property (nonatomic) SKSpriteNode *paddle;
 
 @end
 
+static const uint32_t ballCategory      = 1;    // 00000000000000000000000000000001
+static const uint32_t brickCategory     = 2;    // 00000000000000000000000000000010
+static const uint32_t paddleCategory    = 4;    // 00000000000000000000000000000100
+static const uint32_t edgeCategory      = 8;    // 00000000000000000000000000001000
+
+/*
+ or, we may use bitwise operators
+ static const uint32_t ballCategory      = 0x1;         // 00000000000000000000000000000001
+ static const uint32_t brickCategory     = 0x1 << 1;    // 00000000000000000000000000000010
+ static const uint32_t paddleCategory    = 0x1 << 2;    // 00000000000000000000000000000100
+ static const uint32_t edgeCategory      = 0x1 << 3;    // 00000000000000000000000000001000
+ */
+
 @implementation GameScene
+
+-(void)didBeginContact:(SKPhysicsContact *)contact {
+//    if (contact.bodyA.categoryBitMask == brickCategory) {
+//        NSLog(@"Brick Boing");
+//        [contact.bodyA.node removeFromParent];
+//    }
+    
+    // create placeholder reference for the "non ball" object
+    SKPhysicsBody *notTheBall;
+    if (contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask) {
+        notTheBall = contact.bodyB;
+    } else {
+        notTheBall = contact.bodyA;
+    }
+    
+    if (notTheBall.categoryBitMask == brickCategory) {
+        NSLog(@"Bye brick!");
+        [notTheBall.node removeFromParent];
+    }
+    
+    if (notTheBall.categoryBitMask == paddleCategory) {
+        NSLog(@"Play boing sound");
+    }
+}
 
 - (void)addBall {
     // new sprite from the ball
@@ -27,6 +64,10 @@
     ball.physicsBody.friction = 0;
     ball.physicsBody.linearDamping = 0;
     ball.physicsBody.restitution = 1;
+    ball.physicsBody.categoryBitMask = ballCategory;
+    
+    ball.physicsBody.contactTestBitMask = brickCategory | paddleCategory;
+//    ball.physicsBody.collisionBitMask = edgeCategory | brickCategory;   // only collides with the edge and brick
     
     // add to screen
     [self addChild:ball];
@@ -45,8 +86,9 @@
         // add a static body
         brick.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:brick.frame.size];
         brick.physicsBody.dynamic = NO;
+        brick.physicsBody.categoryBitMask = brickCategory;
         
-        int xPos = self.size.width / 5 * i++;
+        int xPos = self.size.width / 5 * (i+1);
         int yPos = self.size.height - 50;
         brick.position = CGPointMake(xPos, yPos);
         
@@ -63,6 +105,7 @@
     self.paddle.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.paddle.frame.size];
     // make paddle static
     self.paddle.physicsBody.dynamic = NO;
+    self.paddle.physicsBody.categoryBitMask = paddleCategory;
     // add to scene
     [self addChild:self.paddle];
 }
@@ -80,9 +123,11 @@
     self.backgroundColor = [SKColor whiteColor];
     
     self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+    self.physicsBody.categoryBitMask = edgeCategory;
     
     // change gravity settings
     self.physicsWorld.gravity = CGVectorMake(0, 0);
+    self.physicsWorld.contactDelegate = self;   // didBeginContact: & didEndContact:
     
     [self addBall];
     [self addPlayer];
